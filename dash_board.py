47717efe5022837507836e27f99e91b22478d494
BrunoRammon <BrunoRammon>
2022-12-08 18:14:01 -0300

    added some graphics

---
+++ dash_board.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from ast import literal_eval
+import plotly.express as px
+import streamlit as st
+
+# Initial setting up 
+pd.set_option('display.max_columns', 500)
+plt.rcParams['figure.figsize'] = [24, 9]
+# plt.rcParams['font.size'] = 40
+sns.set()
+
+# Functions
